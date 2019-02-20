e21dbe732d71f3069f4a38326c6dcb2bd87ae722
jesusmar <jesusmarlima@gmail.com>
2019-02-19 22:30:31 -0500

    create field code to invitees

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -1,4 +1,11 @@
+require 'digest'
 class Invitee < ApplicationRecord
   belongs_to :group
   validates :name, :email, presence: true
+
+  before_create :generate_code
+
+  def generate_code
+    self.code = Digest::SHA256.base64digest(self.email)[0..20]
+  end
 end
