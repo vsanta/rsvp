912fbbe4ce89129107685b8c58832a36e68203ba
jesusmar <jesusmarlima@gmail.com>
2019-02-19 21:19:06 -0500

    add admin resources

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.5.0'
 
 gem 'rails_admin', '~> 1.3'
 
+gem 'activeadmin'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 # Use sqlite3 as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_02_20_015555) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
