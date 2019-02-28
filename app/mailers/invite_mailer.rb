7d9b3032c603eebc2730e174e9103ba3e750d811
Vanessa Sant'Anna <vanessa.santanna@gmail.com>
2019-02-27 22:15:35 -0500

    ensure metric is only created once

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,7 +2,10 @@ class InviteMailer < ApplicationMailer
   def invite
     @invitee = params[:invitee]
     @url = "#{Rails.application.credentials.invite_domain}/#{@invitee.code}"
-    Metric.create(invitee: @invitee, sent: true)
+    Metric.find_or_initialize_by(invitee: @invitee) do |metric|
+      metric.sent = true
+      metric.save
+    end
     mail(to:  @invitee.email , subject: 'O aniversario da Olivia esta chegando')
   end
 end
