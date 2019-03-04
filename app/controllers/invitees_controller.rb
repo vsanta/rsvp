d79dacb4fc490cb68d908edd38bf0718513c116a
jesusmar <jesusmarlima@gmail.com>
2019-03-04 16:40:19 -0500

    fix form and controller to accept update all invitees insted only selected

--- app/controllers/invitees_controller.rb
+++ app/controllers/invitees_controller.rb
@@ -1,6 +1,7 @@
 class InviteesController < ApplicationController
   def index
     @invitee = Invitee.find_by(code: params[:code])
+    @group = @invitee.group
     Metric.where({:invitee => @invitee}).update_all({received: true})
 
     if @invitee
