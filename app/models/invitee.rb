b58cb1ff3312da541270c51d8d47a64cce828ed8
jesusmar <jesusmarlima@gmail.com>
2019-03-04 16:41:09 -0500

    add csv import

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -1,11 +1,24 @@
 require 'digest'
+require 'csv'
+
 class Invitee < ApplicationRecord
   belongs_to :group
   validates :name, :email, presence: true
-
   before_create :generate_code
 
   def generate_code
-    self.code = Digest::SHA256.base64digest(self.email)[0..20]
+    self.code = Digest::SHA256.base64digest(self.email + self.name)[0..20]
+  end
+
+  def self.import_csv(file)
+    CSV.parse(file, :headers => true, :encoding => 'ISO-8859-1').each do |row|
+      hashed_row = row.to_hash
+      group = Group.find_or_initialize_by(id: hashed_row["group_id"].to_i)
+      if group.new_record?
+        group.name = hashed_row["name"]
+        group.save
+      end
+      Invitee.create!(hashed_row)
+    end
   end
 end
