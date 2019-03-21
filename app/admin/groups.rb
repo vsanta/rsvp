2a9096f9f86fc9b1ea5ed1b40e9ad99fbc911c01
jesusmar <jesusmarlima@gmail.com>
2019-03-20 22:06:48 -0400

    fix email url

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register Group do
   batch_action :invite_all do |ids|
     batch_action_collection.find(ids).each do |group|
       group.invitees.each do |invitee|
-        InviteMailer.with(invitee: invitee).invite.deliver_later if invitee.email
+        InviteMailer.with(invitee: invitee).invite.deliver_later if invitee.email.present?
       end
     end
     redirect_to collection_path, alert: "Invite sent to #{ids.length} groups."
--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,11 +1,11 @@
 class InviteMailer < ApplicationMailer
   def invite
     @invitee = params[:invitee]
-    @url = "#{Rails.application.credentials.invite_domain}/#{@invitee.code}"
+    @url = "#{Rails.application.credentials.invite_domain}#{@invitee.code}"
     Metric.find_or_initialize_by(invitee: @invitee) do |metric|
       metric.sent = true
       metric.save
     end
-    mail(to:  @invitee.email , subject: 'O aniversario da Olivia esta chegando')
+    mail(to:  @invitee.email , subject: 'O aniversário da Olivia está chegando')
   end
 end
